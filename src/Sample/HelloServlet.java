912ee6b967176394dd47c4d6fe5e986d693d338b
Nillouise <LovelyNihilism@outlook.com>
2017-06-26 06:28:15 +0800

    it can use servlet properly

---
+++ src/Sample/HelloServlet.java
@@ -0,0 +1,29 @@
+package Sample;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by win7x64 on 2017/6/26.
+ */
+@WebServlet(name = "HelloServlet",urlPatterns = {"/s"})
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + "hello" + "</h1>");
+
+    }
+}
---
+++ src/Sample/hello.java
@@ -0,0 +1,9 @@
+package Sample;
+
+/**
+ * Created by win7x64 on 2017/6/26.
+ */
+public class hello {
+
+
+}
