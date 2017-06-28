c58dc56cac71c13642e71ac353912e0c63c5b702
Nillouise <LovelyNihilism@outlook.com>
2017-06-28 15:40:22 +0800

    complete mybatis2 advanced

--- mybatis2/src/main/java/com/how2java/pojo/TestMybatis.java
+++ mybatis2/src/main/java/com/how2java/pojo/TestMybatis.java
@@ -2,25 +2,46 @@ package com.how2java.pojo;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import com.how2java.pojo.Category;
+
 public class TestMybatis {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession session=sqlSessionFactory.openSession();
+        SqlSession session = sqlSessionFactory.openSession();
 
-        List<Category> cs=session.selectList("listCategory");
+        Map<String,Object> params = new HashMap<String,Object>();
+        params.put("id", 3);
+        params.put("name", "cat");
+
+        List<Category> cs = session.selectList("listCategoryByIdAndName",params);
+        for (Category c : cs) {
+            System.out.println(c.getName());
+            System.out.println(c.getId());
+            System.out.println("fdsfds5");
+        }
+
+        session.commit();
+        session.close();
+
+    }
+    private static void listAll(SqlSession session) {
+        List<Category> cs = session.selectList("listCategory");
         for (Category c : cs) {
             System.out.println(c.getName());
         }
-
     }
+
 }
